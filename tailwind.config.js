593de678b30d09398951e10cf4be45f03bed39be
Sarah CJ <sarah4594@gmail.com>
2024-02-20 15:52:52 -0600

    Add custom colors and test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
+		colors: {
+			blue: {
+				DEFAULT: "#001d5d",
+				light: "#b8bfcf",
+			},
+			green: {
+				DEFAULT: "#42a418",
+				light: "#d8f0d0",
+			},
+			gray: {
+				DEFAULT: "#777777",
+				light: "#f6f6f6",
+			},
+			black: "#000000",
+		},
 		extend: {},
 	},
 	plugins: [],

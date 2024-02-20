94a7a9f60a8f70b53bc1ed0a04dcaacef8971d77
Sarah CJ <sarah4594@gmail.com>
2024-02-20 15:25:27 -0600

    Start work on header
    add names and icons

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
-import HeroImage from "./components/HeroImage.vue";
-import MainContent from "./components/MainContent.vue";
-import NewReleases from "./components/NewReleases.vue";
-import Map from "./components/Map.vue";
-import ContactForm from "./components/ContactForm.vue";
-import Footer from "./components/Footer.vue";
+// import HeroImage from "./components/HeroImage.vue";
+// import MainContent from "./components/MainContent.vue";
+// import NewReleases from "./components/NewReleases.vue";
+// import Map from "./components/Map.vue";
+// import ContactForm from "./components/ContactForm.vue";
+// import Footer from "./components/Footer.vue";
 </script>
 
 <template>
 	<Header />
-	<HeroImage />
+	<!-- <HeroImage />
 	<MainContent />
 	<NewReleases />
 	<Map />
 	<ContactForm />
-	<Footer />
+	<Footer /> -->
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,33 @@
 <template>
-	<div>Header</div>
+	<div id="nav" className="sticky top-0 flex justify-between m-10 uppercase">
+		<div>
+			<img src="../../public/Logos/Logo-Blue.svg" alt="Logo-Blue" />
+		</div>
+		<router-link to="/" class="flex flex-row items-center">
+			<img src="../../public/Icons/Nav Icons/Home.svg" alt="Home" />
+			<p class="text-md px-2">Home</p>
+		</router-link>
+		<router-link to="/records" class="flex flex-row items-center">
+			<img src="../../public/Icons/Nav Icons/Record.svg" alt="Record" />
+			<p class="text-md px-2">Records</p>
+		</router-link>
+		<router-link to="/music" class="flex flex-row items-center">
+			<img src="../../public/Icons/Nav Icons/Music Note.svg" alt="Music Note" />
+			<p class="text-md px-2">Music</p>
+		</router-link>
+		<router-link to="/artists" class="flex flex-row items-center">
+			<img src="../../public/Icons/Nav Icons/People.svg" alt="People" />
+			<p class="text-md px-2">Artists</p>
+		</router-link>
+		<router-link to="/concerts" class="flex flex-row items-center">
+			<img src="../../public/Icons/Nav Icons/Calendar.svg" alt="Calendar" />
+			<p class="text-md px-2">Concerts</p>
+		</router-link>
+		<router-link to="/signup">
+			<button class="bg-green-200 py-2 px-8 rounded">
+				Sign Up
+			</button></router-link
+		>
+	</div>
+	<router-view />
 </template>

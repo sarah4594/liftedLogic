03ccaf3218549e69240c72878d971a6b4d993d8d
Sarah CJ <sarah4594@gmail.com>
2024-02-20 19:46:16 -0600

    Take out flex-row (unnecessary) and edit play button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,23 +6,23 @@
 		<div>
 			<img src="/Logos/Logo-Blue.svg" alt="Logo-Blue" />
 		</div>
-		<router-link to="/" class="flex flex-row items-center">
+		<router-link to="/" class="flex flex items-center">
 			<img src="/Icons/Nav Icons/Home.svg" alt="Home" />
 			<p class="text-md px-2">Home</p>
 		</router-link>
-		<router-link to="/records" class="flex flex-row items-center">
+		<router-link to="/records" class="flex flex items-center">
 			<img src="/Icons/Nav Icons/Record.svg" alt="Record" />
 			<p class="text-md px-2">Records</p>
 		</router-link>
-		<router-link to="/music" class="flex flex-row items-center">
+		<router-link to="/music" class="flex flex items-center">
 			<img src="/Icons/Nav Icons/Music Note.svg" alt="Music Note" />
 			<p class="text-md px-2">Music</p>
 		</router-link>
-		<router-link to="/artists" class="flex flex-row items-center">
+		<router-link to="/artists" class="flex flex items-center">
 			<img src="/Icons/Nav Icons/People.svg" alt="People" />
 			<p class="text-md px-2">Artists</p>
 		</router-link>
-		<router-link to="/concerts" class="flex flex-row items-center">
+		<router-link to="/concerts" class="flex flex items-center">
 			<img src="/Icons/Nav Icons/Calendar.svg" alt="Calendar" />
 			<p class="text-md px-2">Concerts</p>
 		</router-link>

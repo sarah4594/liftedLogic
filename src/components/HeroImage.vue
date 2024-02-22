4785c4373d32c65aff2858f9f7c6747c48a79aa1
Sarah CJ <sarah4594@gmail.com>
2024-02-21 22:47:53 -0600

    Formatting and add animation to image

--- src/components/HeroImage.vue
+++ src/components/HeroImage.vue
@@ -1,25 +1,35 @@
 <template>
-	<div class="relative flex flex-wrap items-center">
+	<div class="relative w-full aspect-[1270/656] overflow-clip">
 		<img
-			class="max-h-[655px] w-full object-cover"
 			src="/Images/marcela-laskoski-YrtFlrLo2DQ-unsplash.jpg"
-			alt="Marcela Laskoski"
+			alt="Hero Image"
+			class="object-cover w-full h-full animate-breathe"
 		/>
-		<div class="absolute text-white pl-36 max-w-[708px]">
-			<h1 class="text-5xl my-10 font-bold">A.M Paradox</h1>
-			<p class="text-base">
-				Christopher Brent Wood, better known by his stage name Brent Faiyaz, is
-				an American singer and record producer. He released his debut project,
-				and extended play entitled A.M. Paradox, in 2016
-			</p>
-			<div class="flex flex my-10 items-center">
-				<button class="rounded-full h-16 w-16 bg-white p-5">
-					<img
-						src="/Icons/BasicIcons/Play button triangle.svg"
-						alt="Play Button"
-					/>
-				</button>
-				<p class="text-base font-semibold px-8">Watch Video</p>
+		<div
+			class="absolute top-0 left-0 flex flex-col justify-center w-full h-full bg-black bg-opacity-35"
+		>
+			<div class="ml-32">
+				<h1 class="text-6xl font-bold text-white">A.M. Paradox</h1>
+				<p class="text-white text-lg mt-10 w-[60%]">
+					Christopher Brent Wood, better known by his stage name Brent Faiyaz,
+					is an American singer and record producer. He released his debut
+					project, an extended play entitled A.M. Paradox, in 2016.
+				</p>
+				<div class="flex items-center gap-6 mt-6">
+					<button
+						class="inline-flex items-center justify-center w-16 h-16 text-black bg-white rounded-full hover:bg-gray-light hover:text-red-500"
+					>
+						<svg
+							class="flex-none w-6 h-6"
+							viewBox="0 0 20 20"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path d="M20 10L5 18.6603L5 1.33974L20 10Z" fill="currentColor" />
+						</svg>
+					</button>
+					<span class="text-lg font-bold text-white">Watch Video</span>
+				</div>
 			</div>
 		</div>
 	</div>

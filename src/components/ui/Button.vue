6b9ad41aa74ee3d73a8d86e38b7ae4190fdba4c3
Sarah CJ <sarah4594@gmail.com>
2024-02-21 22:45:32 -0600

    Add button component

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,22 @@
+<template>
+	<button
+		:type="type"
+		class="px-8 py-2 transition-colors duration-200 ease-in-out rounded-sm bg-green-light text-green hover:bg-green hover:text-green-light"
+	>
+		<slot />
+	</button>
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+
+export default {
+	name: "Button",
+	props: {
+		type: {
+			type: String as PropType<"button" | "submit" | "reset">,
+			default: "button",
+		},
+	},
+};
+</script>

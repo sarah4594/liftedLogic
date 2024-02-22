da668af614a8803c4fa8e9dbcf99bc262f532b7b
Sarah CJ <sarah4594@gmail.com>
2024-02-22 14:38:46 -0600

    Add responsiveness and change background color

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,6 +1,7 @@
 <template>
-	<div class="w-full aspect-[1270/540] bg-gray-100">
+	<div class="w-full sm:aspect-[1270/540] bg-gray-light">
 		<img
+			class="object-cover w-full aspect-square sm:aspect-auto"
 			src="https://maps.googleapis.com/maps/api/staticmap?center=38.9872578,-94.6709722&zoom=15&size=635x270&scale=2&maptype=roadmap&markers=color:green%7C38.9872578,-94.6709722&key=AIzaSyAh2L9CksbAqNkqwq7cPgELzgW2e0W9YjA&map_id=7d4b220f6986a2ca"
 		/>
 	</div>

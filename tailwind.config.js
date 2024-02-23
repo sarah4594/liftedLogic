41ce2496190e4393a6896032cb102abd55263581
Sarah CJ <sarah4594@gmail.com>
2024-02-23 12:18:55 -0600

    Change where custom colors are defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,26 @@ import defaultTheme from "tailwindcss/defaultTheme";
 export default {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		colors: {
-			blue: {
-				DEFAULT: "#001d5d",
-				light: "#b8bfcf",
-			},
-			green: {
-				DEFAULT: "#42a418",
-				light: "#d8f0d0",
-			},
-			gray: {
-				DEFAULT: "#777777",
-				light: "#f6f6f6",
-			},
-			black: "#000000",
-			white: "#ffffff",
-		},
 		extend: {
 			fontFamily: {
 				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				blue: {
+					DEFAULT: "#001d5d",
+					light: "#b8bfcf",
+				},
+				green: {
+					DEFAULT: "#42a418",
+					light: "#d8f0d0",
+				},
+				gray: {
+					DEFAULT: "#777777",
+					light: "#f6f6f6",
+				},
+				black: "#000000",
+				white: "#ffffff",
+				transparent: "transparent",
 			},
 			animation: {
 				breathe: "breathe 3s ease-in-out infinite",

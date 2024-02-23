d37b13d705a9784a80e09ebd6daf31019cf6ddd2
Sarah CJ <sarah4594@gmail.com>
2024-02-22 21:19:03 -0600

    Add responsiveness

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -3,15 +3,17 @@
 		class="flex bg-[url('/Images/harry-swales-0l0KjNoAfAk-unsplash.jpg')] bg-center bg-cover lg:bg-none"
 	>
 		<div class="flex flex-row bg-white bg-opacity-85">
-			<div class="basis-1/2 border-b border-gray/10 py-12 px-12">
-				<h2 class="leading-7 text-4xl text-blue font-bold">Get in Touch</h2>
-				<p class="pt-8 leading-6 text-gray tracking-wide">
+			<div class="lg:basis-1/2 border-b border-gray/10 py-12 px-12">
+				<h2 class="leading-7 text-4xl sm:text-4xl/10 text-blue font-bold">
+					Get in Touch
+				</h2>
+				<p class="pt-8 sm:mt-8 sm:mr-32 leading-6 text-gray tracking-wide">
 					New albums every single month, check out the newest & best from Snyder
 					Recording artist, now available on Apple Music & Spotify.
 				</p>
 
 				<form
-					class="py-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
+					class="py-10 grid grid-cols-1 gap-x-6 gap-y-8"
 					method="post"
 					@submit="
 						(e) => {
@@ -19,54 +21,44 @@
 						}
 					"
 				>
-					<div class="sm:col-span-1">
-						<label
-							for="first-name"
-							class="block py-1 text-sm leading-6 text-gray"
-							>First name</label
-						>
-						<div>
+					<div class="flex flex-col gap-4 sm:gap-10 sm:flex-row">
+						<div class="flex-1">
+							<label for="firstName" class="block text-gray">First Name</label>
 							<input
 								type="text"
 								name="first-name"
 								placeholder="First Name"
 								id="first-name"
 								autocomplete="given-name"
+								required
 								class="block w-full border-0 py-4 pl-4 text-blue shadow-sm ring-1 ring-inset ring-gray placeholder:text-gray focus:ring-1 focus:ring-inset focus:ring-blue sm:leading-6"
 							/>
 						</div>
-					</div>
-
-					<div>
-						<label
-							for="last-name"
-							class="block py-1 text-sm leading-6 text-gray"
-							>Last name</label
-						>
-						<div>
+						<div class="flex-1">
+							<label for="lastName" class="block text-gray">Last Name</label>
 							<input
 								type="text"
 								name="last-name"
 								placeholder="Last Name"
 								id="last-name"
 								autocomplete="family-name"
+								required
 								class="block w-full border-0 py-4 pl-4 text-blue shadow-sm ring-1 ring-inset ring-gray placeholder:text-gray focus:ring-1 focus:ring-inset focus:ring-blue sm:leading-6"
 							/>
 						</div>
 					</div>
 
-					<div>
-						<label for="email" class="block py-1 text-sm leading-6 text-gray"
-							>Email address</label
-						>
-						<div>
+					<div class="flex flex-col gap-4 sm:gap-10 sm:flex-row">
+						<div class="flex-1">
+							<label for="email" class="block text-gray">Email Address</label>
 							<input
 								id="email"
 								name="email"
 								placeholder="Email"
 								type="email"
 								autocomplete="email"
-								class="block w-full border-0 py-4 pl-4 text-blue shadow-sm ring-1 ring-inset ring-gray placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-blue sm:leading-6"
+								required
+								class="block w-full border-0 py-4 pl-4 text-blue shadow-sm ring-1 ring-inset ring-gray placeholder:text-gray focus:ring-1 focus:ring-inset focus:ring-blue sm:leading-6"
 							/>
 						</div>
 						<div class="flex-1">
@@ -97,9 +89,10 @@
 							<textarea
 								id="message"
 								name="message"
+								class="block w-full border-0 py-4 pl-4 text-blue shadow-sm ring-1 ring-inset ring-gray placeholder:text-gray focus:ring-1 focus:ring-inset focus:ring-blue sm:leading-6"
 								placeholder="Write a brief message..."
-								rows="3"
-								class="block w-full h-32 px-4 py-4 mt-2 text-blue shadow-sm ring-1 ring-inset ring-gray placeholder:text-gray focus:ring-1 focus:ring-inset focus:ring-blue sm:leading-6"
+								required
+								minlength="10"
 							></textarea>
 						</div>
 						<div class="mt-8">
